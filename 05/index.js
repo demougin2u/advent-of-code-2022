d3306cbd372bbaa1fcab1dbaba64c9c5494e05c1
Dylan Demougin <dylan.demougin@boursorama.fr>
2022-12-05 19:36:47 +0100

    feat(day5): part I

---
+++ 05/index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const textInput = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+let [stacksInputs, movesInputs] = textInput.split('\n\n').map((input) => input.split('\n'));
+const nbColumns = stacksInputs.pop().replace(/ /g, '').split('').length;
+const stacksInputsSanitized = stacksInputs.map(s => s.replace(/[\[\]]/g, '').split(/\s{0,3}\s/g));
+
+const stacks = stacksInputsSanitized.reduceRight(
+        (arr, inputs) => {
+        inputs.forEach((value, stackIndex) => {
+            if (value !== '') {
+                arr[stackIndex].push(value);
+            }
+        });
+        return arr;
+    },
+    Array.from({length: nbColumns}, (_) => [])
+);
+
+const moves = movesInputs.map(move => {
+    const [,numberOfItems,,fromNumber,,toNumber] = move.split(' ').map(i => parseInt(i));
+    return {
+        numberOfItems,
+        from: fromNumber - 1,
+        to: toNumber - 1
+    };
+});
+
+
+/** PART I */
+console.log('---------- PART I ----------');
+const partOne = (originalStacks, moves) => {
+    let stacks = JSON.parse(JSON.stringify(originalStacks));
+    moves.forEach(move => {
+        for (let i = 0; i < move.numberOfItems; i++) {
+            stacks[move.to].push(stacks[move.from].pop())
+        }
+    });
+
+    return stacks.reduce((result, stack) => `${result}${stack.pop()}`, '');
+};
+
+console.log(partOne(stacks, moves));
+
+
+/** PART II */
+console.log('---------- PART II ----------');
+
+const partTwo = (stacks, moves) => '';
+
+console.log(partTwo(stacks, moves));
